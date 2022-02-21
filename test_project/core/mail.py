3b2710f497519279f7acdf61bfb639865b228dae
SmileElly <ellysmileday.gmail.com>
2022-02-21 12:26:26 +0200

    jinja is not required for mailhog

--- test_project/core/mail.py
+++ test_project/core/mail.py
@@ -1,41 +1,18 @@
+import logging
 import smtplib
-
-from jinja2 import Template
 
 
 class SendMail:
     def __init__(self):
         self.email_url = "mailhog:1025"
 
-    def create_email_body(
-        self, email_params: dict, template_text: str, template_params: dict
-    ):
-        template = Template(template_text)
-        email_body = {
-            "personalizations": email_params.get("personalizations"),
-            "from": email_params.get("from"),
-            "reply_to": email_params.get("reply_to"),
-            "subject": email_params.get("subject"),
-            "content": [
-                {
-                    "type": "text/html",
-                    "value": template.render(
-                        **template_params,
-                    ),
-                }
-            ],
-            "categories": email_params.get("categories"),
-        }
-
-        return email_body
-
     def send_request(self, data):
         try:
             smtpObj = smtplib.SMTP(self.email_url)
             smtpObj.sendmail("sender", "reciever", data)
-            print("Successfully sent email")
+            logging.info("Successfully sent email")
         except Exception:
-            print("Error: unable to send email")
+            logging.warning("Error: unable to send email")
 
     def send_notification_mail(
         self, user_email: str, data_mail: dict
